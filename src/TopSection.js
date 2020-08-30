e789adefe83a98ef720ff7382aa3b8d501197293
emmalynnn97 <emmalynnn97@gmail.com>
2020-08-30 14:39:23 -0700

    fix scroll to distance

--- src/TopSection.js
+++ src/TopSection.js
@@ -52,7 +52,7 @@ export class TopSection extends Component {
             <div style={containerStyle}>
                 <h1 className='top-section-heading' style={largeHeadingStyle}>Hi, I'm <span style={specialTagStyle}>Emma Lynn</span>,<br/>a front end web <span style={specialTagStyle}>developer</span><br/>and <span style={specialTagStyle}>designer</span>.</h1>
                 <div className='view-more-box' onClick={()=>{
-                    window.scrollTo({ top: 1000, behavior: 'smooth' });
+                    window.scrollTo({ top: 750, behavior: 'smooth' });
                 }}  style={{cursor:'pointer',textAlign:'center',position:'relative',top:'5em'}}><h2 className='view-more-text' style={viewMoreStyle}>View More</h2><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-arrow-down-circle" width="52" height="52" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#FF65DE" fill="none" strokeLinecap="round" strokeLinejoin="round">
   <path stroke="none" d="M0 0h24v24H0z"/>
   <circle cx="12" cy="12" r="9" />
