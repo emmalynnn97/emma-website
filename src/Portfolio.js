7c5feac42198a4841c7aa2d77db5077928f56aaf
emmalynnn97 <emmalynnn97@gmail.com>
2020-09-07 11:23:38 -0700

    yay portfolio page

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -11,10 +11,31 @@ export class Portfolio extends Component {
     }
     
     render() {
+        const containerStyle={
+            display:'grid',
+            gridGap:'10px',
+            gridTemplateColumns:'auto auto auto auto',
+            marginTop:'10vh',
+            height:'100vh',
+            padding:'5em',
+        }
+        const projectStyle={
+
+        }
+        const projectOverlayStyle={
+            width:'100%',
+            height:'100%',
+            backgroundColor:'rgba(240, 220, 253, .1)'
+        }
         return (
             <div>
                 <Nav offset={2} links={[<Link className='link home-link' style={{color:'#383838',textDecoration:'none'}} to='/'>Home</Link>,<Link className='link about-link' style={{color:'#383838',textDecoration:'none'}} to='/about'>About</Link>,<Link className='link portfolio-link' style={{color:'#FF65DE',textDecoration:'none',fontWeight:'600'}} to='/portfolio'>Portfolio</Link>]}/>
-                Portfolio Page
+                <div style={containerStyle} className='project-container'>
+                    <div style={projectStyle} className='project project-1'><div style={projectOverlayStyle} className='project-overlay'></div></div>
+                    <div style={projectStyle} className='project project-2'><div style={projectOverlayStyle} className='project-overlay'></div></div>
+                    <div style={projectStyle} className='project project-3'><div style={projectOverlayStyle} className='project-overlay'></div></div>
+                    <div style={projectStyle} className='project project-4'><div style={projectOverlayStyle} className='project-overlay'></div></div>
+                </div>
             </div>
         )
     }
